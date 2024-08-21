c21405611e59766f46446b73d1257d29b1ab771b
Ofek Baribi <baribi9@gmail.com>
2024-08-21 14:51:57 +0300

    Removed old Web files before re-adding subtree

--- Server/README.md
+++
@@ -1,1 +0,0 @@
-# ViewTube-Server
--- Web/src/app/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('check if hello world exists', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello world/i);
-  expect(linkElement).toBeInTheDocument();
-});
